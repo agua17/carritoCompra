07f957e9d64e579a9c1704737bdd24c757f70597
Agua <aguassantasnunez@gmail.com>
2025-01-24 12:48:19 +0100

    funcion_leer_datos_curso

--- script.js
+++ script.js
@@ -13,6 +13,22 @@ function agregarCurso(e){
     e.preventDefault();
 
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target)
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
+}
+
+function leerDatosCurso(curso){
+    console.log(curso);
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id:curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(infoCurso);
+
 }
